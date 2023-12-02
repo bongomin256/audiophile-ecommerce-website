aca8210e82464ca21f8b114fa9bb94f19248a6ca
Godfrey Bongomin <98063606+bongomin256@users.noreply.github.com>
2023-12-01 18:51:31 -0800

    installed hookform/devtools

--- src/pages/checkoutPage/BillingDetail.js
+++ src/pages/checkoutPage/BillingDetail.js
@@ -4,6 +4,12 @@ import { useForm } from "react-hook-form";
 const BillingDetail = () => {
   // invoking useform function
   const form = useForm();
+
+  //   Destructuring the register method from useForm.
+  const { register } = form; //use for tracking the form state
+
+  // const { name, ref, onChange, onBlur } = register("username");
+
   return (
     <section>
       <h4>billing details</h4>
@@ -11,15 +17,15 @@ const BillingDetail = () => {
         <form action="">
           <div>
             <label htmlFor="username">name</label>
-            <input type="text" name="username" />
+            <input type="text" {...register("username")} />
           </div>
           <div>
             <label htmlFor="email">email</label>
-            <input type="email" name="email" />
+            <input type="email" {...register("email")} />
           </div>
           <div>
             <label htmlFor="phone">phone number</label>
-            <input type="tel" name="phone" />
+            <input type="tel" {...register("phone")} />
           </div>
         </form>
       </div>
