752aa2186f2ac8801a636345a437d56e9b8c3b7b
Godfrey Bongomin <98063606+bongomin256@users.noreply.github.com>
2023-12-26 15:28:50 -0800

    fixed styling in YX1

--- src/components/HomeComponents/YX1EarphoneHome/Yx1Earphones.js
+++ src/components/HomeComponents/YX1EarphoneHome/Yx1Earphones.js
@@ -5,20 +5,20 @@ import { ActionButtonOuline } from "../../Shared/ActionButtons";
 import { Link } from "react-router-dom";
 const Yx1Earphones = () => {
   return (
-    <section className="px-8 mt-8 md:px-16 lg:px-40">
+    <section className="w-full px-8 mt-8 md:px-16 lg:px-40">
       <article className="md:grid md:grid-cols-2 md:items-center md:gap-5 ">
         <picture>
           <source media="(min-width: 1024px )" srcset={yx1Desktop} />
           <source media="(min-width:768px )" srcset={yx1Tablet} />
           <img
-            className="rounded-lg h-[200px] w-[320px] md:h-[320px] md:w-[339px] lg:w-full "
+            className="rounded-lg h-[200px] w-[320px] md:m-h-[320px] md:m-w-[339px] md:h-full md:w-full lg:w-full "
             src={yx1Mobile}
             alt=""
           />
         </picture>
-        <section className="py-10 mt-8 h-[200px]    md:h-[320px] md:w-[339px] lg:w-full  rounded-lg bg-gray-30 md:mt-0 md:py-24 md:px-12 lg:px-20">
-          {/* w-full md:h-full */}
-          <div className="pl-5">
+        <section className="py-10 mt-8 h-[200px] md:m-h-[320px] md:m-w-[339px] md:h-full md:w-full lg:w-full  rounded-lg bg-gray-30 md:mt-0  md:px-12 lg:px-20">
+          {/* w-full md:h-full  md:py-24*/}
+          <div className="pl-5 md:pl-0">
             <h2 className="text-4xl text-black uppercase font-[500] mb-6 md:mb-8 md:text-3xl">
               yx1 earphones
             </h2>
