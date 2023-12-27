14e078488b8f29a7c3ecf6feeea07afb3a67bfd4
Godfrey Bongomin <98063606+bongomin256@users.noreply.github.com>
2023-12-26 17:23:50 -0800

    adjusted width in y17 and yx1

--- src/components/HomeComponents/ZX7SpeakerHome/Zx7Speaker.js
+++ src/components/HomeComponents/ZX7SpeakerHome/Zx7Speaker.js
@@ -4,7 +4,8 @@ import { Link } from "react-router-dom";
 function Zx7Speaker() {
   return (
     <section className="w-full px-8 mt-8 md:px-16 lg:px-40">
-      <article className="relative h-[320px] rounded-lg bg-no-repeat bg-contain bg-mobileZx7Home md:bg-tabletZx7Home lg:bg-desktopZx7Home lg:bg-cover">
+      <article className="relative h-[320px]  rounded-lg bg-no-repeat bg-cover md:bg-contain bg-mobileZx7Home md:bg-tabletZx7Home lg:bg-desktopZx7Home lg:bg-cover">
+        {/* bg-contain */}
         <div className="absolute top-[25%] pl-5 md:pl-14 lg:pl-24">
           <h2 className="mb-10 text-4xl font-[500] text-black uppercase">
             Zx7 Speaker
